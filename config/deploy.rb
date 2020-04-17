af9dbc58fc2ac49d5ae17b095aa9339cb4938283
miyayamaM <pendulum.ngt.4010@gmail.com>
2020-04-17 21:23:50 +0900

    capistrano setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,30 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.13.0'
 
+# Capistranoのログの表示に利用する
 set :application, 'eureka'
-#アプリ名を記載
-set :repo_url, 'git@github.com:miyayamaM/eureka.git'
-#ただし、configファイルでgithub.com→githubとして置き換えられているので注意
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:miyayamaM/eureka.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all
 
-set :log_level, :warn
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml')
-set :linked_files, fetch(:linked_files, []).push('config/master.key')
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-set :keep_releases, 3
-
-set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
-
-set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
-
-namespace :deploy do
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-
-    end
-  end
-end
-
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
 end
-
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,42 +1,55 @@
-APP_PATH   = "#{File.dirname(__FILE__)}/.." unless defined?(APP_PATH)
-RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
-RAILS_ENV  = ENV['RAILS_ENV'] || 'development'
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../', __FILE__)
 
-worker_processes 3
+#アプリケーションサーバの性能を決定する
+worker_processes 1
 
-listen "/tmp/unicorn.sock"
-pid "tmp/pids/unicorn.pid"
+#アプリケーションの設置されているディレクトリを指定
+working_directory app_path
+
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+#ポート番号を指定
+listen 3000
+
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+#Railsアプリケーションの応答を待つ上限時間を設定
+timeout 60
+
+#以下は応用的な設定なので説明は割愛
 
 preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-timeout 60
-working_directory APP_PATH
+check_client_connection false
 
-# logのpath
-stderr_path "#{RAILS_ROOT}/log/unicorn_error.log"
-stdout_path "#{RAILS_ROOT}/log/unicorn_access.log"
-
-if GC.respond_to?(:copy_on_write_friendly=)
-  GC.copy_on_write_friendly = true
-end
-
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = APP_PATH + "/Gemfile"
-end
+run_once = true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
 
-  old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid == server.pid
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
     end
   end
 end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
