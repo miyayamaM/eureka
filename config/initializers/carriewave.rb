12024658d08a1119a9e6d28b3eb2dbb02859eacf
miyayamaM <pendulum.ngt.4010@gmail.com>
2021-07-11 17:49:18 +0900

    carriewave設定変更

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -2,11 +2,12 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAUK4LYSXCJQI746X3',
-      aws_secret_access_key: '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',
+      aws_access_key_id: Rails.application.credentials.aws[:s3_access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_access_key],
       region: 'ap-northeast-1'
     }
 
+    config.fog_public  = false
     config.fog_directory  = 'bucket-eureeeeka'
     config.cache_storage = :fog
   end
